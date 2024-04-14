9c6be927e743101dc1e3b81edd7be84d04ff3376
fnx4 <allllleex911@gmail.com>
2024-04-14 12:01:13 +0300

    20240414

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 ```
 usage: silero_tts.py [-h] -i INPUT [-o OUTPUT] [-t THREADS] [-s {aidar,baya,kseniya,xenia,eugene}]
                      [-d {cpu,cuda,xpu,opengl,opencl,ideep,vulkan,hpu}] [-r RATE] [--merge] [--rvc]
+                     [--rvc_model_pth RVC_MODEL_PTH] [--rvc_model_index RVC_MODEL_INDEX]
+                     [--rvc_transpose RVC_TRANSPOSE]
 
 tts
 
@@ -23,8 +25,14 @@ optional arguments:
                         torch.device value (default: cpu)
   -r RATE, --rate RATE  sample rate (default: 48000)
   --merge               [FFmpeg required] merge wav files and save as opus (default: False)
-  --rvc                 [FFmpeg required] [cuda only] use voice conversion (Retrieval-based-Voice-Conversion 0814v2)
+  --rvc                 [FFmpeg required] [cuda only] use voice conversion (Retrieval-based-Voice-Conversion v2.2)
                         (default: False)
+  --rvc_model_pth RVC_MODEL_PTH
+                        RVC model: .pth file name (required) (default: ru-saya-1000.pth)
+  --rvc_model_index RVC_MODEL_INDEX
+                        RVC model: .index file name (default: None)
+  --rvc_transpose RVC_TRANSPOSE
+                        RVC model: f0 transpose (default: 3)
 
 Example: ./silero_tts.py -i folder/or/file.txt -o path/to/result -t 16 -s xenia -d cuda -r 48000 --merge
 ```
--- silero_tts.py
+++ silero_tts.py
@@ -4,12 +4,13 @@ import os
 import re
 import argparse
 import subprocess
+from sys import exit
 
 import torch # cuda: 1.13.1+cu117
 import nltk
 import transliterate
 import num2words
-import tqdm
+from tqdm.auto import tqdm
 import pypandoc
 import ffmpeg # ffmpeg-python
 
@@ -39,6 +40,9 @@ class Cfg:
         self.rate = param["rate"]
         self.merge = param["merge"]
         self.rvc = param["rvc"]
+        self.rvc_model_pth = param["rvc_model_pth"]
+        self.rvc_model_index = param["rvc_model_index"]
+        self.rvc_transpose = param["rvc_transpose"]
 
 
 class MergeParameters:
@@ -102,7 +106,7 @@ def main(args):
         if cfg.merge:
             encode(cfg, merge_objects)
     else:
-        print("Error: input file or folder not found")
+        error("Error: input file or folder not found")
         exit(1)
     print("\nFinished!")
     exit(0)
@@ -136,7 +140,7 @@ def read_file(cfg: Cfg, model, merge_objects, in_file_path, out_file_path):
     elif in_file_path.lower().endswith(".fb2"):
         md = pypandoc.convert_file(in_file_path, "md")
     else:
-        print("Error: unknown  file  extension")
+        error("Error: unknown file extension")
         exit(1)
 
     chapters = []
@@ -150,12 +154,12 @@ def read_file(cfg: Cfg, model, merge_objects, in_file_path, out_file_path):
             chapter_text += line + "\n"
             if line == "":
                 chapter_text += "\r\n"
-            if cfg.rvc and len(chapter_text.encode("utf-8")) > RVC_VRAM_LIMIT * 3 * 1000:
+            if cfg.rvc and len(chapter_text.encode("utf-8")) > RVC_VRAM_LIMIT * 6 * 1000:
                 chapters.append(chapter_text)
                 chapter_text = " \r\n"
     chapters.append(chapter_text)
 
-    for line_num, chapter in enumerate(chapters):
+    for line_num, chapter in enumerate(tqdm(chapters, position=0, desc=" CHAPTER")):
         chapter_name = str(chapter).partition("\n")[0] if is_ebook else os.path.basename(in_file_path)
         chapter_name = pypandoc.convert_text(chapter_name, "plain", format="md")
         chapter_name = chapter_name.replace(" ", "_").replace("\r", "").replace("\n", "")
@@ -173,8 +177,6 @@ def read_file(cfg: Cfg, model, merge_objects, in_file_path, out_file_path):
         if not os.path.exists(out_chapter_path):
             os.makedirs(out_chapter_path)
         if is_ebook:
-            print()
-            print(out_chapter_path)
             volume_name = os.path.basename(out_file_path)
         else:
             volume_name = ""
@@ -188,9 +190,7 @@ def read_file(cfg: Cfg, model, merge_objects, in_file_path, out_file_path):
 
 
 def tts(cfg: Cfg, model, lines, out_file_path):
-    # print(lines)
-    # print(out_file_path)
-    for line_num, text in enumerate(tqdm.tqdm(lines, desc=" TTS")):
+    for line_num, text in enumerate(tqdm(lines, leave=False, position=1, desc="    TEXT")):
         text = str(text).replace("…", ",")
         text = str(text).replace("+", " плюс ")
         text = str(text).replace("%", " процент ")
@@ -200,7 +200,7 @@ def tts(cfg: Cfg, model, lines, out_file_path):
         for sentence in t_sentences:
             sentences += re.findall('.{1,800}', sentence)
         # print(sentences)
-        for sentence_num, sentence in enumerate(sentences):
+        for sentence_num, sentence in enumerate(tqdm(sentences, leave=False, position=2, desc="SENTENCE")):
             file_name = os.path.join(out_file_path, "tts_" + str(line_num).zfill(6) + "_" + str(sentence_num).zfill(3) + ".wav")
             sentence = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0)), lang="ru"), sentence)
             # print(sentence)
@@ -263,7 +263,7 @@ def encode(cfg: Cfg, merge_objects):
     # print(concatenated_wav_files)
     if cfg.rvc:
         if cfg.device != "cuda":
-            print("ERROR, device " + cfg.device + " is not supported")
+            error("Error: device " + cfg.device + " is not supported")
             exit(1)
 
         rvc_path = os.path.join(os.path.abspath(os.getcwd()), "rvc")
@@ -276,14 +276,12 @@ def encode(cfg: Cfg, merge_objects):
         else:
             vc_python_bin = "python"
 
-        model_name = "ru-saya"
-        model_pth = "ru-saya-1000.pth"
-        model_index = "trained_IVF3468_Flat_nprobe_1_ru-saya_v2.index"
+        model = " --model_name " + cfg.rvc_model_pth # ./<rvc_path>/assets/weights
+        if cfg.rvc_model_index is not None:
+            model += " --index_path" + cfg.rvc_model_index
 
-        transpose = "3"
-
-        for file in tqdm.tqdm(concatenated_wav_files):
-            if os.path.getsize(file.wav_file_path) > (RVC_VRAM_LIMIT * 10 * 1024 * 1024):  # ~3KB of text (~10MB of wav) for each GB of VRAM
+        for file in tqdm(concatenated_wav_files):
+            if os.path.getsize(file.wav_file_path) > (RVC_VRAM_LIMIT * 20 * 1024 * 1024):  # ~6KB of text (~20MB of wav) for each GB of VRAM
                 wrn_text = "WARNING: File is too large, high VRAM usage: " + file.wav_out_path
                 wrn.append(wrn_text)
                 print(wrn_text)
@@ -292,15 +290,23 @@ def encode(cfg: Cfg, merge_objects):
             rvc_out_path = os.path.join(file.root_path, "_wav_rvc", file.volume)
             os.makedirs(rvc_out_path, exist_ok=True)
             rvc_out_file_path = os.path.join(rvc_out_path, file.file_base_name + ".wav")
-            rvc_params = "" + transpose + " " \
-                         "\"" + file.wav_file_path + "\" " \
-                         "\"" + rvc_out_file_path + "\" " \
-                         "\"" + os.path.join(rvc_path, "models", model_name, model_pth) + "\" " \
-                         "\"" + os.path.join(rvc_path, "models", model_name, model_index) + "\" " \
-                         "" + "\"cuda:0\" " \
-                         "" + "\"rmvpe\" "
-            proc = vc_python_bin + " " + os.path.join(rvc_path, "infer_cli.py") + " " + rvc_params
-            subprocess.run(proc, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            rvc_params = model + \
+                         " --f0up_key " + cfg.rvc_transpose + \
+                         " --input_path " + os.path.join(os.getcwd(), file.wav_file_path) + \
+                         " --opt_path " + os.path.join(os.getcwd(), rvc_out_file_path) + \
+                         " --device cuda:0" + \
+                         " --f0method rmvpe" + \
+                         " --filter_radius 0" + \
+                         " --index_rate 0" + \
+                         " --rms_mix_rate 0.4"
+            proc = vc_python_bin + " " + os.path.join(rvc_path, "tools", "infer_cli.py") + " " + rvc_params
+            process = subprocess.run(proc, cwd=rvc_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+            if not os.path.exists(rvc_out_file_path):
+                print()
+                print("RVC output file not found: ")
+                error(process.stdout.decode("utf-8"))
+                exit(1)
 
             opus_out_path = os.path.join(file.root_path, "_opus", file.volume)
             opus_out_file_path = os.path.join(opus_out_path, file.file_base_name + ".opus")
@@ -310,13 +316,17 @@ def encode(cfg: Cfg, merge_objects):
             stream_opus = ffmpeg.output(stream_opus, opus_out_file_path, acodec="libopus", audio_bitrate="32k", loglevel="error")
             ffmpeg.run(stream_opus, overwrite_output=True)
     else:
-        for file in tqdm.tqdm(concatenated_wav_files):
+        for file in tqdm(concatenated_wav_files):
             opus_volume_folder_path = os.path.join(file.root_path, "_opus", file.volume)
             os.makedirs(opus_volume_folder_path, exist_ok=True)
             opus_out_path = os.path.join(opus_volume_folder_path, file.file_base_name + ".opus")
             stream_opus = ffmpeg.input(file.wav_file_path)
             stream_opus = ffmpeg.output(stream_opus, opus_out_path, acodec="libopus", audio_bitrate="32k", loglevel="error")
             ffmpeg.run(stream_opus, overwrite_output=True)
+
+
+def error(err):
+    print("\033[91m" + err + "\033[0m")
 
 
 if __name__ == "__main__":
@@ -329,7 +339,10 @@ if __name__ == "__main__":
                         choices=["cpu", "cuda", "xpu", "opengl", "opencl", "ideep", "vulkan", "hpu"])
     parser.add_argument("-r", "--rate", action="store", help="sample rate", default="48000")
     parser.add_argument("--merge", action="store_true", help="[FFmpeg required] merge wav files and save as opus")
-    parser.add_argument("--rvc", action="store_true", help="[FFmpeg required] [cuda only] use voice conversion (Retrieval-based-Voice-Conversion 0814v2)")
+    parser.add_argument("--rvc", action="store_true", help="[FFmpeg required] [cuda only] use voice conversion (Retrieval-based-Voice-Conversion v2.2)")
+    parser.add_argument("--rvc_model_pth", action="store", help="RVC model: .pth file name (required)", default="ru-saya-1000.pth")
+    parser.add_argument("--rvc_model_index", action="store", help="RVC model: .index file name")
+    parser.add_argument("--rvc_transpose", action="store", help="RVC model: f0 transpose", default="3")
     # TODO RVC vram target/limit
     # TODO print debug logs
 
